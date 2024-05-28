4e6aaba413d7dcd4cefd3dba2a84cb5e6a71441b
JeehoonYYY <enfetter@naver.com>
2024-05-28 15:23:06 +0900

    Delete Chapter05 directory

--- Chapter05/src/homework/Person.java
+++
@@ -1,9 +0,0 @@
-package homework;
-
-public class Person {
-	int personAge;
-	String personName;
-	boolean isMarried;
-	int children;	
-}
-
--- Chapter05/src/reference/Student1.java
+++
@@ -1,9 +0,0 @@
-package reference;
-
-public class Student1 {
-	int studentId;
-	String studentName;
-	int koreaScore;
-	int mathScore;
-	
-}
